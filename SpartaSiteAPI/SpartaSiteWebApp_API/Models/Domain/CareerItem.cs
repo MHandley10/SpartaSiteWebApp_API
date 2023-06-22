94d7dcab162e422aee08f525b8bd5428b869f1a8
unloosedcascade <matthewdan.handley@btinternet.com>
2023-06-22 11:33:29 +0100

    added careeritem class

---
+++ SpartaSiteAPI/SpartaSiteWebApp_API/Models/Domain/CareerItem.cs
@@ -0,0 +1,15 @@
+﻿namespace SpartaSiteWebApp_API.Models.Domain;
+
+public class CareerItem
+{
+	public Guid Id { get; set; }
+	public string Title { get; set; }
+	public string Description { get; set; }
+	public decimal Salary { get; set; }
+	public DateTime PostDate { get; set; }
+	public DateTime CloseDate { get; set; }
+	public bool IsFilled { get; set; } = false;
+	public ICollection<User>? Users { get; set; }
+	public Guid AuthorSpartanId { get; set; }
+	public Spartan Author { get; set; }
+}
