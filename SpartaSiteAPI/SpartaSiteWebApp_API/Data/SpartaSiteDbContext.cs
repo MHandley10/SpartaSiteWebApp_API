0ec76994d59b5e7f0b97c4a26a7ffa229071479a
unloosedcascade <matthewdan.handley@btinternet.com>
2023-06-22 13:29:10 +0100

    added a dbContext class

---
+++ SpartaSiteAPI/SpartaSiteWebApp_API/Data/SpartaSiteDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpartaSiteWebApp_API.Models.Domain;
+
+namespace SpartaSiteWebApp_API.Data;
+
+public class SpartaSiteDbContext : DbContext
+{
+	public SpartaSiteDbContext(DbContextOptions<SpartaSiteDbContext> dbContextOptions) : base(dbContextOptions)
+	{
+
+	}
+
+	public DbSet<CareerItem> CareerItems { get; set; }
+	public DbSet<Course> Courses { get; set; }
+	public DbSet<CV> CVs { get; set; }
+	public DbSet<EnquiringCompany> EnquiringCompanies { get; set; }
+	public DbSet<NewsItem> NewsItems { get; set; }
+	public DbSet<QuestionBank> Questions { get; set; }
+	public DbSet<Spartan> Spartans { get; set; }
+	public DbSet<User> Users { get; set; }
+	public DbSet<VideoResources> Videos { get; set; }
+}
