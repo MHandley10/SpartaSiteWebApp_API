66e0a3c16e1a11cc2041c346b1eda4ebb2b958be
unloosedcascade <matthewdan.handley@btinternet.com>
2023-06-22 10:56:21 +0100

    added the user class

--- SpartaSiteAPI/SpartaSiteWebApp_API/Models/Domain/NewsItem.cs
+++ SpartaSiteAPI/SpartaSiteWebApp_API/Models/Domain/NewsItem.cs
@@ -4,6 +4,7 @@ namespace SpartaSiteWebApp_API.Models.Domain;
 
 public class NewsItem
 {
+	[Key]
 	public Guid Id { get; set; }
 	public string Content { get; set; }
 	public string Author { get; set; }
---
+++ SpartaSiteAPI/SpartaSiteWebApp_API/Models/Domain/User.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace SpartaSiteWebApp_API.Models.Domain;
+
+public class User
+{
+	[Key]
+	public Guid Id { get; set; }
+	public string FirstName { get; set; }
+	public string? MiddleName { get; set; }
+	public string LastName { get; set; }
+	public DateTime DateOfBirth { get; set; }
+	public DateTime DateJoined { get; set; }
+	public bool IsSpartan { get; set; } = false;
+	public string Address { get; set; }
+	public string PostCode { get; set; }
+	public string CountryOfResidence { get; set; }
+	public string? Title { get; set; }
+	public string ContactNumber { get; set; }
+	public string Email { get; set; }
+	public Guid SpartanId { get; set; }
+	public Spartan Spartan { get; set; }
+
+	public ICollection<CareerItem> { get; set;}
+}
