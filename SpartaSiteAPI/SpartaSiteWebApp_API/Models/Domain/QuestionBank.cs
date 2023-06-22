663fc47f5675669ff3ac4a6abd7549a6805cb0ec
unloosedcascade <matthewdan.handley@btinternet.com>
2023-06-22 12:07:09 +0100

    added and completed the questionbank class

---
+++ SpartaSiteAPI/SpartaSiteWebApp_API/Models/Domain/QuestionBank.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SpartaSiteWebApp_API.Models.Domain;
+
+public class QuestionBank
+{
+	[Key]
+	public Guid Id { get; set; }
+	public string Question { get; set; }
+	public string Answer { get; set; }
+	public string? Comments { get; set; }
+	public string Author { get; set; }
+	[ForeignKey("Course")]
+	public string CourseType { get; set; }
+	public string QuestionTopic { get; set; }
+	public DateTime DateUploaded { get; set; }
+}
