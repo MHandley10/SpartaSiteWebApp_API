823ee4429295d5d740aac0654beb9217d8c18485
unloosedcascade <matthewdan.handley@btinternet.com>
2023-06-22 10:45:34 +0100

    added NewsItem class

---
+++ SpartaSiteAPI/SpartaSiteWebApp_API/Models/Domain/NewsItem.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpartaSiteWebApp_API.Models.Domain;
+
+public class NewsItem
+{
+	public Guid Id { get; set; }
+	public string Content { get; set; }
+	public string Author { get; set; }
+	public string Title { get; set; }
+	public string Date { get; set; }
+}
