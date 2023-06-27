2bbbaa3e965dbe0c290a84ece561cb38a6ec5977
unloosedcascade <matthewdan.handley@btinternet.com>
2023-06-27 08:54:55 +0100

    created empty API controllers for all data classes

---
+++ SpartaSiteAPI/SpartaSiteWebApp_API/Controllers/SpartanController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpartaSiteWebApp_API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class SpartanController : ControllerBase
+	{
+	}
+}
