28b40728086d3f205f05794751ca3dfcb8b4cdde
unloosedcascade <matthewdan.handley@btinternet.com>
2023-07-06 12:17:43 +0100

    added an interface called ICareerItemRepository for making a careerItem repository

---
+++ SpartaSiteAPI/SpartaSiteWebApp_API/Repositories/ICareerItemRepository.cs
@@ -0,0 +1,12 @@
+﻿using SpartaSiteWebApp_API.Models.Domain;
+
+namespace SpartaSiteWebApp_API.Repositories;
+
+public interface ICareerItemRepository
+{
+	Task<CareerItem> CreateAsync(CareerItem careerItem);
+	Task<CareerItem> GetAllAsync();
+	Task<CareerItem?> GetByIdAsync(Guid id);
+	Task<CareerItem?> UpdateAsync(Guid id, CareerItem careerItem);
+	Task<CareerItem?> DeleteAsync(Guid id);
+}
