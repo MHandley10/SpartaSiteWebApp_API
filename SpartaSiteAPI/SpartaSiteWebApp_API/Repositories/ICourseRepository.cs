d93d8ff59ac5f98b023902f06641822bc2e7e469
unloosedcascade <matthewdan.handley@btinternet.com>
2023-07-10 16:12:31 +0100

    completed course repository interface

--- SpartaSiteAPI/SpartaSiteWebApp_API/Repositories/ICourseRepository.cs
+++ SpartaSiteAPI/SpartaSiteWebApp_API/Repositories/ICourseRepository.cs
@@ -1,5 +1,12 @@
-﻿namespace SpartaSiteWebApp_API.Repositories;
+﻿using SpartaSiteWebApp_API.Models.Domain;
+
+namespace SpartaSiteWebApp_API.Repositories;
 
 public interface ICourseRepository
 {
+	Task<List<Course>> GetAllAsync();
+	Task<Course?> GetByIdAsync(Guid id);
+	Task<Course> CreateAsync(Course course);
+	Task<Course?> UpdateAsync(Guid id, Course course);
+	Task<Course?> DeleteAsync(Guid id);
 }
