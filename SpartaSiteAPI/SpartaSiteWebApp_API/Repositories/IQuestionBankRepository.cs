22f63f483aad039483ca73e6302a09d1989c6cac
unloosedcascade <matthewdan.handley@btinternet.com>
2023-07-10 16:13:35 +0100

    completed question bank repository interface

--- SpartaSiteAPI/SpartaSiteWebApp_API/Repositories/IQuestionBankRepository.cs
+++ SpartaSiteAPI/SpartaSiteWebApp_API/Repositories/IQuestionBankRepository.cs
@@ -1,5 +1,12 @@
-﻿namespace SpartaSiteWebApp_API.Repositories;
+﻿using SpartaSiteWebApp_API.Models.Domain;
+
+namespace SpartaSiteWebApp_API.Repositories;
 
 public interface IQuestionBankRepository
 {
+	Task<List<Question>> GetAllAsync();
+	Task<Question?> GetByIdAsync(Guid id);
+	Task<Question> CreateAsync(Question question);
+	Task<Question?> UpdateAsync(Guid id, Question question);
+	Task<Question?> DeleteAsync(Guid id);
 }
