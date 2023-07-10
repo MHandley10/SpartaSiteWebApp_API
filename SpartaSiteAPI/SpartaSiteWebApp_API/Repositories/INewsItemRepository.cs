21545e4b5dcfc452b4b2d20a6cebe147e9e67d4a
unloosedcascade <matthewdan.handley@btinternet.com>
2023-07-10 16:11:42 +0100

    completed news item repository interface

--- SpartaSiteAPI/SpartaSiteWebApp_API/Repositories/INewsItemRepository.cs
+++ SpartaSiteAPI/SpartaSiteWebApp_API/Repositories/INewsItemRepository.cs
@@ -1,5 +1,12 @@
-﻿namespace SpartaSiteWebApp_API.Repositories;
+﻿using SpartaSiteWebApp_API.Models.Domain;
+
+namespace SpartaSiteWebApp_API.Repositories;
 
 public interface INewsItemRepository
 {
+	Task<List<NewsItem>> GetAllAsync();
+	Task<NewsItem?> GetByIdAsync(Guid id);
+	Task<NewsItem> CreateAsync(NewsItem newsItem);
+	Task<NewsItem?> UpdateAsync(Guid id, NewsItem newsItem);
+	Task<NewsItem?> DeleteAsync(Guid id);
 }
