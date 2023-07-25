8b88e83d4a81b3de08ac0e3bef6bbd2de8d1991a
unloosedcascade <matthewdan.handley@btinternet.com>
2023-07-25 13:51:30 +0100

    added NewsItemRepository and completed courseRepository

---
+++ SpartaSiteAPI/SpartaSiteWebApp_API/Repositories/NewsItemRepository.cs
@@ -0,0 +1,29 @@
+﻿namespace SpartaSiteWebApp_API.Repositories;
+
+public class NewsItemRepository : INewsItemRepository
+{
+	public Task<NewsItemRepository> CreateAsync(NewsItemRepository newsItem)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<NewsItemRepository?> DeleteAsync(Guid id)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<List<NewsItemRepository>> GetAllAsync()
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<NewsItemRepository?> GetByIdAsync(Guid id)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<NewsItemRepository?> UpdateAsync(Guid id, NewsItemRepository newsItem)
+	{
+		throw new NotImplementedException();
+	}
+}
